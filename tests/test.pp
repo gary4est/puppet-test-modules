47191c8d801695b333e12098a3fc30e189ef32f6
Gary Forrest <gary@kountable.com>
2019-04-16 18:00:58 -0600

    fix syntax on test.pp

--- tests/test.pp
+++ tests/test.pp
@@ -1,9 +1,11 @@
 
 
-$num = regsubst($hostname, '^\w+(\d{3})\w+$','\1') notify { "host num is ${num}": }  # $i3 = regsubst($ipaddress,'^(\d+)\.(\d+)\.(\d+)\.(\d+)$','\3')
+$num = regsubst($hostname, '^\w+(\d{3})\w+$','\1')
+notify { "host num is ${num}": }
+
+# $i3 = regsubst($ipaddress,'^(\d+)\.(\d+)\.(\d+)\.(\d+)$','\3')
 
 $region = regsubst($ec2_placement_availability_zone, '^(\w+)\-(\w+)\-(\d)\w','\1-\2-\3')
-
 notify { "Region is ${region}": }
 
 $jvar = "jdk1.7.0_71-x86_64.tgz"
@@ -12,3 +14,5 @@ $jdkver = regsubst($jvar,"-${architecture}.tgz$", '\1')
 $jdktype = regsubst($jvar,'^(\w{3}).+$','\1')
 
 notify { "jvar is ${jvar} jdktype is ${jdktype} jdkver is ${jdkver}": }
+
+
